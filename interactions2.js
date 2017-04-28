c28654fbd34f2fddaafb42b368aa2ead7563ee4c
SherryH <enjoy.wind7rain@gmail.com>
2017-04-29 10:26:54 +1200

    Drew grid lines

--- interactions2.js
+++ interactions2.js
@@ -2,6 +2,7 @@ const MYAPP = {
   hasSecondPlayer: null,
   playerOneSymbol: null,
   playerTwoSymbol: null,
+  timeOuts: [],
   initializeGame: function() {
     /* Game choice page */
     $('.game-choice button').click(function(){
@@ -13,6 +14,7 @@ const MYAPP = {
     /* Game starter page */
     $('.game-starter .role-control button').click(function() {
       MYAPP.game.startGame(this);
+
     });
 
     $('.back-button').click(function() {
@@ -38,6 +40,82 @@ MYAPP.display = {
   hideGameStarter: function() {
     return $('.game-starter').fadeOut(600);
   },
+  showGameBoard: function() {
+    $('.game-board').fadeIn(400);
+    MYAPP.display.drawBoard();
+    $('#myCanvas').fadeIn(400);
+
+  },
+  drawBoard: function() {
+    /*show game board*/
+    var c = document.getElementById("myCanvas");
+    var canvas = c.getContext("2d");
+    canvas.lineWidth = 1;
+    canvas.strokeStyle = "#fff";
+    //vertical lines
+    canvas.beginPath();
+    canvas.moveTo(100, 0);
+    canvas.lineTo(100, 146.5);
+    canvas.closePath();
+    canvas.stroke();
+    canvas.beginPath();
+    canvas.moveTo(200, 0);
+    canvas.lineTo(200, 146.5);
+    canvas.closePath();
+    canvas.stroke();
+
+    // horizontal lines
+    canvas.lineWidth = 0.5;
+
+    canvas.beginPath();
+    canvas.moveTo(4, 48.5);
+    canvas.lineTo(296, 48.5);
+    canvas.closePath();
+    canvas.stroke();
+
+    canvas.beginPath();
+    canvas.moveTo(4, 98.5);
+    canvas.lineTo(296, 98.5);
+    canvas.closePath();
+    canvas.stroke();
+
+// setTimeout(function() {
+
+//   }, 1500)
+  //   MYAPP.timeOuts.push(setTimeout(function() {
+  //   var c = document.getElementById("myCanvas");
+  //   var canvas = c.getContext("2d");
+  //   canvas.lineWidth = 1;
+  //   canvas.strokeStyle = "#fff";
+  //   //vertical lines
+  //   canvas.beginPath();
+  //   canvas.moveTo(100, 0);
+  //   canvas.lineTo(100, 146.5);
+  //   canvas.closePath();
+  //   canvas.stroke();
+  //   canvas.beginPath();
+  //   canvas.moveTo(200, 0);
+  //   canvas.lineTo(200, 146.5);
+  //   canvas.closePath();
+  //   canvas.stroke();
+
+  //   // horizontal lines
+  //   canvas.lineWidth = 0.5;
+
+  //   canvas.beginPath();
+  //   canvas.moveTo(4, 48.5);
+  //   canvas.lineTo(296, 48.5);
+  //   canvas.closePath();
+  //   canvas.stroke();
+
+  //   canvas.beginPath();
+  //   canvas.moveTo(4, 98.5);
+  //   canvas.lineTo(296, 98.5);
+  //   canvas.closePath();
+  //   canvas.stroke();
+  // }, 1500));
+  },
+
 };
 
 MYAPP.game = {
@@ -47,6 +125,16 @@ MYAPP.game = {
   startGame: function(playerOneSymbol) {
     MYAPP.playerOneSymbol = $(playerOneSymbol).text();
     console.log('playerOne', MYAPP.playerOneSymbol);
+    MYAPP.playerTwoSymbol = (MYAPP.playerOneSymbol==='X')? 'X': 'O';
+    /* Game board page */
+    // work on 2 player logic first
+    // draw game baord
+    // create player score card, reset button to game choice page, player turn tilt
+    $.when(MYAPP.display.hideGameStarter())
+    .done(MYAPP.display.showGameBoard);
+
+    // MYAPP.display.drawBoard();
+
   },
 };
 
