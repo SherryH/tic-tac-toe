93af4fbd0ecb9639fef1638bd29b578feac8b525
SherryH <enjoy.wind7rain@gmail.com>
2017-04-29 10:47:22 +1200

    Draw reset button

--- interactions2.js
+++ interactions2.js
@@ -78,42 +78,6 @@ MYAPP.display = {
     canvas.lineTo(296, 98.5);
     canvas.closePath();
     canvas.stroke();
-
-// setTimeout(function() {
-
-//   }, 1500)
-  //   MYAPP.timeOuts.push(setTimeout(function() {
-  //   var c = document.getElementById("myCanvas");
-  //   var canvas = c.getContext("2d");
-  //   canvas.lineWidth = 1;
-  //   canvas.strokeStyle = "#fff";
-  //   //vertical lines
-  //   canvas.beginPath();
-  //   canvas.moveTo(100, 0);
-  //   canvas.lineTo(100, 146.5);
-  //   canvas.closePath();
-  //   canvas.stroke();
-  //   canvas.beginPath();
-  //   canvas.moveTo(200, 0);
-  //   canvas.lineTo(200, 146.5);
-  //   canvas.closePath();
-  //   canvas.stroke();
-
-  //   // horizontal lines
-  //   canvas.lineWidth = 0.5;
-
-  //   canvas.beginPath();
-  //   canvas.moveTo(4, 48.5);
-  //   canvas.lineTo(296, 48.5);
-  //   canvas.closePath();
-  //   canvas.stroke();
-
-  //   canvas.beginPath();
-  //   canvas.moveTo(4, 98.5);
-  //   canvas.lineTo(296, 98.5);
-  //   canvas.closePath();
-  //   canvas.stroke();
-  // }, 1500));
   },
 
 };
@@ -133,7 +97,6 @@ MYAPP.game = {
     $.when(MYAPP.display.hideGameStarter())
     .done(MYAPP.display.showGameBoard);
 
-    // MYAPP.display.drawBoard();
 
   },
 };
