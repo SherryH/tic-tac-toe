bd499c57f762248d441a8280f56b6fbd99e1ea2e
SherryH <enjoy.wind7rain@gmail.com>
2017-05-20 20:46:47 +1200

    Created outer game container in React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+//outer gamebox container
 const App = () => {
   return (
-    <div>
-      Hello Beautiful World :D
+    <div className="outer-container">
+      <div className="board-container">
+        <div className="game-choice">
+          <p>How do you want to play?</p>
+          <div className="role-control">
+            <button>One Player</button>
+            <button>Two Player</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
