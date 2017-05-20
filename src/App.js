b213467f071a83e09ea13210cb022965f6623b80
SherryH <enjoy.wind7rain@gmail.com>
2017-05-21 08:49:00 +1200

    Added Router for enabling page change

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React from 'react';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 
 //outer gamebox container
+//implement transition, then refactor the outer container to a separate app
+//get these down in stateless components first with transition
+//use redux to keep track of the states
 const App = () => {
-  return (
-    <div className="outer-container">
-      <div className="board-container">
-        <div className="game-choice">
-          <p>How do you want to play?</p>
-          <div className="role-control">
-            <button>One Player</button>
-            <button>Two Player</button>
+  const enterGameStarter = () => {
+    {console.log('clicked')}
+    return (
+      <CSSTransitionGroup
+        transitionName="game-starter"
+        transitionEnterTimeout={600}
+        transitionLeaveTimeout={600}>
+        <div>Example Text testing fading</div>
+      </CSSTransitionGroup>
+    );
+  };
+  //put game-choice into a function, like how states are rendered
+  //use transition to render the function
+  const createGameChoice = () => {
+    return (
+      <CSSTransitionGroup
+        transitionName="game-choice"
+        transitionEnterTimeout={600}
+        transitionLeaveTimeout={600}>
+        <div className="outer-container">
+          <div className="board-container">
+            <div className="game-choice">
+              <p>How do you want to play?</p>
+              <div className="role-control">
+                <button>One Player</button>
+                <button onClick={enterGameStarter}>Two Players</button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </CSSTransitionGroup>
+    );
+  };
+
+  return (
+    <div>
+    {createGameChoice()}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {IndexRoute, Router, Route, hashHistory} from 'react-router';
 import App from './App';
 
 
 render(
+  <Router history={hashHistory}>
+    <Route path="/" component = {App}>
+      <IndexRoute component={GameChoice}>
+      <Route path="gamestart" component={GameStart}/>
+    </Route>
+  </Router>
   <App />,
   document.getElementById('app')
 );
