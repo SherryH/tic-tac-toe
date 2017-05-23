fc46e851a21ff6e5b51b0ff6f7662669e4060697
SherryH <enjoy.wind7rain@gmail.com>
2017-05-24 09:03:38 +1200

    Enabled link for navigating from gamechoice to gamestart

--- src/components/GameChoice.js
+++ src/components/GameChoice.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const GameChoice = () =>{
 
@@ -9,7 +10,7 @@ const GameChoice = () =>{
           <p>How do you want to play?</p>
           <div className="role-control">
             <button>One Player</button>
-            <button>Two Players</button>
+            <button><Link to="/gamestart">Two Players</Link></button>
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {BrowserRouter as Router, Route, hashHistory} from 'react-router-dom';
+import {BrowserRouter as Switch, Route, hashHistory} from 'react-router-dom';
 import App from './App';
 import GameChoice from './components/GameChoice';
 import GameStart from './components/GameStart';
@@ -11,11 +11,11 @@ const ExampleApp = () =>(
 );
 
 render(
-  <Router>
+  <Switch>
     <div>
       <Route exact path="/" component = {GameChoice} />
       <Route path="/gamestart" component={GameStart} />
     </div>
-  </Router>,
+  </Switch>,
   document.getElementById('app')
 );
