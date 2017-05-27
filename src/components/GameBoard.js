154711d50a9403df87849a1dcc56624f54c9eb3c
SherryH <enjoy.wind7rain@gmail.com>
2017-05-28 09:16:10 +1200

    Enabled drawing gameboard

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,16 +1,59 @@
 import React from 'react';
 
-const GameBoard = () =>{
+export default class GameBoard extends React.Component {
+  constructor(props){
+    super(props);
+    this.drawBoard = this.drawBoard.bind(this);
+  }
+  componentDidMount(){
+    this.drawBoard();
+  }
 
-  return (
-        <div className="board-container">
-          <div className="game-board">
-            <canvas id="myCanvas"></canvas>
-            <div className="boxes"></div>
+  drawBoard (){
+    /*show game board*/
+    var c = document.getElementById("myCanvas");
+    var canvas = c.getContext("2d");
+    canvas.lineWidth = 1;
+    canvas.strokeStyle = "#fff";
+    //vertical lines
+    canvas.beginPath();
+    canvas.moveTo(100, 0);
+    canvas.lineTo(100, 146.5);
+    canvas.closePath();
+    canvas.stroke();
+    canvas.beginPath();
+    canvas.moveTo(200, 0);
+    canvas.lineTo(200, 146.5);
+    canvas.closePath();
+    canvas.stroke();
+
+    // horizontal lines
+    canvas.lineWidth = 0.5;
+
+    canvas.beginPath();
+    canvas.moveTo(4, 48.5);
+    canvas.lineTo(296, 48.5);
+    canvas.closePath();
+    canvas.stroke();
+
+    canvas.beginPath();
+    canvas.moveTo(4, 98.5);
+    canvas.lineTo(296, 98.5);
+    canvas.closePath();
+    canvas.stroke();
+  }
+
+  render(){
+    return (
+          <div className="board-container">
+            <div className="game-board">
+              <canvas id="myCanvas"></canvas>
+              <div className="boxes"></div>
+            </div>
           </div>
-        </div>
 
-  );
-};
+    );
+  }
+}
 
-export default GameBoard;
+
---
+++ src/configureStore.js
@@ -0,0 +1,12 @@
+import {createStore} from 'redux';
+import { createLogger } from 'redux-logger';
+
+//this configures the store that saves states
+
+const configureStore = () =>{
+  //it takes in the reducer and create Store
+  //it can also take in middlewares like redux logger for debuggiing
+
+};
+
+export default configureStore;
