6c8567b2ee11cee05fbf5fe9a3ccbfb24a7fe6f4
SherryH <enjoy.wind7rain@gmail.com>
2017-06-03 11:16:49 +1200

    Added react-router-redux library for redirection

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,5 +1,6 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import { createLogger } from 'redux-logger';
+import { routerReducer, routerMiddleware } from 'react-router-redux';
 
 //this configures the store that saves states
 
@@ -7,8 +8,17 @@ const configureStore = () =>{
   //it takes in the reducer to subscribe to the dispatched action
   //it can also take in middlewares like redux logger for debuggiing
 
+  // Create a history of your choosing (we're using a browser history in this case)
+  const history = createHistory();
+
+  // Build the middleware for intercepting and dispatching navigation actions
+  const routerM = routerMiddleware(history);
+
   //logs will be on even in production environment
-  return createStore(reducer, applyMiddleware(createLogger));
+  return createStore(combineReducers({
+    reducer,
+    router: routerReducer,
+  }), applyMiddleware(createLogger, routerM));
 
 };
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {BrowserRouter as Router, Switch} from 'react-router-dom';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import { logger } from 'redux-logger';
 import {Provider} from 'react-redux';
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
 import AppContainer from './AppContainer';
 import reducer from './reducer/reducer';
 
@@ -13,19 +15,29 @@ const ExampleApp = () =>(
   <div>Example app</div>
 );
 
+// Create a history of your choosing (we're using a browser history in this case)
+const history = createHistory();
+
+// Build the middleware for intercepting and dispatching navigation actions
+const routerM = routerMiddleware(history);
+
 //store contains all states, subscribes to the actions by taking in reducer
-const store = createStore(reducer,applyMiddleware(logger));
+const store = createStore(combineReducers({
+  reducer,
+  router: routerReducer,
+}),applyMiddleware(logger, routerM));
+
 console.log('index',store);
 
 render(
   <Provider store={store}>
-    <Router>
+    <ConnectedRouter history={history}>
       <div className="center-container">
         <div className="outer-container">
             <AppContainer/>
         </div>
       </div>
-    </Router>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('app')
 );
